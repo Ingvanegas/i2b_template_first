5134483a5ec47108d9f801159e527775956b8a4a
Walter vanegas Barceló <waltervanegasbarcelo@gmail.com>
2018-04-03 17:22:15 -0500

    tarde 03-04-2018

--- js/script.js
+++ js/script.js
@@ -3,25 +3,52 @@ $('.menu-toggle').click(function() {
   $(this).toggleClass('open');  
 });
 
-$('.owl-carousel').owlCarousel({
+$('#first_carousel').owlCarousel({
   loop: true,
   margin: 10,
+  dots:false,
   nav: true,
   navText: [
     "<i class='fa fa-angle-left'></i>",
     "<i class='fa fa-angle-right'></i>"
   ],
-  autoplay: false,
+  autoplay: true,
   autoplayHoverPause: true,
   responsive: {
     0: {
       items: 1
     },
+    700: {
+      items: 3
+    },
+    1400: {
+      items: 5
+    }
+  }
+});
+
+$('#second_carousel').owlCarousel({
+  loop: true,
+  autoplay: true,
+  dots: true,
+  autoplayHoverPause: true,
+  items: 1
+});
+
+$('#third_carousel').owlCarousel({
+  loop: true,
+  autoplay: true,
+  dots: true,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
     600: {
-      items: 2
+      items: 3,
     },
     1000: {
-      items: 5
+      items: 6,
     }
   }
 });
