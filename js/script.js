efeeda6e8b3cd4f4848cee809c359b1565004f12
IngVanegas <waltervanegasbarcelo@gmail.com>
2018-04-02 21:05:55 -0500

    02-04-2018

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
 $('.menu-toggle').click(function() {  
   $('.nav').toggleClass('nav-open', 500);
   $(this).toggleClass('open');  
-})
+});
+
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  navText: [
+    "<i class='fa fa-angle-left'></i>",
+    "<i class='fa fa-angle-right'></i>"
+  ],
+  autoplay: true,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 2
+    },
+    1000: {
+      items: 4
+    }
+  }
+});
